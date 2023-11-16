038a94d1fb60a833cdec2a075057cf7ead649485
molangning <133737702+molangning@users.noreply.github.com>
2023-11-17 00:35:49 +0800

    moved file back

---
+++ scripts/ipv4-checker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import re,os
+
+dirs = ['sources/','blacklists/','whitelists/']
+files = []
+IPV4_REGEX=r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\/(3[0-2]|[1-2][0-9]|[1-3])$"
+
+for i in dirs:
+    for j in os.listdir(i):
+        files.append(os.path.join(i,j))
+
+for i in files:
+    f=open(i)
+    contents=f.read()
+
+    if len(contents) == 0:
+        continue # Empty file
+
+    for j in contents.split('\n'):
+        if not re.match(IPV4_REGEX,j):
+            print("[!] %s did not pass the regex check"%(j))
+            exit(2)
+
+print("IPv4 pre push hook check succeeded")
+exit(0)
