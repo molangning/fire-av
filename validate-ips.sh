a57f3f59208a40bda8095a393da3e8811e57f047
molangning <133737702+molangning@users.noreply.github.com>
2023-11-17 02:37:18 +0800

    added commands that exit on command error

--- validate-ips.sh
+++ validate-ips.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# https://stackoverflow.com/questions/3822621/how-to-exit-if-a-command-failed
+
+set -e 
+set -o pipefail
 
 ./scripts/ipv4-checker.py
 ./scripts/new-line-checker.py
