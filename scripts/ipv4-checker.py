549766bd8aadd8081bae0e243f62977c57248f0e
molangning <133737702+molangning@users.noreply.github.com>
2023-11-17 02:07:17 +0800

    Improved logging

--- scripts/ipv4-checker.py
+++ scripts/ipv4-checker.py
@@ -2,6 +2,8 @@
 
 import re,os
 
+print("[+] IPv4 regex check")
+
 dirs = ['sources/','blacklists/','whitelists/']
 files = []
 IPV4_REGEX=r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\/(3[0-2]|[1-2][0-9]|[1-3])$"
@@ -23,7 +25,7 @@ for i in files:
         if not re.match(IPV4_REGEX,j):
             print("[!] %s did not pass the regex check"%(j))
             exit(2)
-    print("[+] %s passed the check"%(i))
+    # print("[+] %s passed IPv4 regex check"%(i))
 
-print("IPv4 checks succeeded")
+print("[+] All IPv4 checks succeeded")
 exit(0)
