02e3b1f3763c3d35929fc777df19be6dea55e3ea
molangning <133737702+molangning@users.noreply.github.com>
2023-11-17 18:40:40 +0800

    added script that checks for valid ipv6 cidr

---
+++ scripts/ipv6-checker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import re,os
+
+print("[+] IPv6 regex check")
+
+files = []
+dirs = ['sources/','blacklists/','whitelists/']
+IPV4_REGEX=r"^((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]d|1dd|[1-9]?d)(.(25[0-5]|2[0-4]d|1dd|[1-9]?d)){3}))|:)))(%.+)?s*(\/([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-8]))$"
+
+for i in dirs:
+    for root,_,file_list in os.walk(i):
+        for file in file_list:
+            if 'ipv6' not in file:
+                continue
+            files.append(os.path.join(root,file))
+
+for i in files:
+    f=open(i)
+    contents=f.read()
+
+    if len(contents) == 0:
+        continue # Empty file
+
+    for j in contents.split('\n'):
+
+        if not re.match(IPV4_REGEX,j,flags=re.IGNORECASE):
+            print("[!] %s did not pass the IPv6 regex check"%(j))
+            print("[!] Offending file: %s"%(i))
+            exit(2)
+
+    print("[+] %s passed IPv6 regex check"%(i))
+
+print("[+] All IPv4 checks succeeded")
+exit(0)
