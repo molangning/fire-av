36e06c079c6890faa16ccefdc176ea43c056e55c
Mo Langning <133737702+molangning@users.noreply.github.com>
2023-11-19 22:14:25 +0800

    updated workflow files

--- commit.sh
+++ commit.sh
@@ -8,10 +8,11 @@ if [ -z "$(git ls-files --modified sources/)" ]; then
     echo "[+] No files were changed"
 else
     echo "[+] Files were changed! Pushing changed..."
+    git remote set-url origin "https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY"
     git stage sources/
     git config --local user.email "example@github.com"
     git config --local user.name "GitHub Action"
-    git commit -m "Automated data update"
+    git commit -m "[Github Action] Automated lists update."
     git push
 fi
 
