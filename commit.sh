502288e9a591a77ab9db928652426a10fbaf852c
Mo Langning <133737702+molangning@users.noreply.github.com>
2023-11-19 22:25:09 +0800

    chunked the entire script into data-updater.yml

--- commit.sh
+++ commit.sh
@@ -8,8 +8,8 @@ if [ -z "$(git ls-files --modified sources/)" ]; then
     echo "[+] No files were changed"
 else
     echo "[+] Files were changed! Pushing changed..."
-    git remote set-url origin "https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY"
     git stage sources/
+    git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY
     git config --local user.email "example@github.com"
     git config --local user.name "GitHub Action"
     git commit -m "[Github Action] Automated lists update."
