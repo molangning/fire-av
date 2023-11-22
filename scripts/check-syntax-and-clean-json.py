f01d28aba0c6c2efd13314e103f65cdfefff3c2d
molangning <133737702+molangning@users.noreply.github.com>
2023-11-22 11:25:33 +0800

    changed regex formats and added validators

---
+++ scripts/check-syntax-and-clean-json.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import re,os,json
+
+print("[+] JSON file syntax check")
+
+dirs=["sources/regexps"]
+regexps_files=[]
+
+for i in dirs:
+    for root,_,file_list in os.walk(i):
+        for file in file_list:
+            if '.json' not in file:
+                continue
+            regexps_files.append(os.path.join(root,file))
+
+for i in regexps_files:
+
+    try:
+        regex_json=json.load(open(i))
+    except Exception as e:
+        print("[!] Error in decoding file %s!"%(i))
+        print("[!] Exception: %s"%(e.msg))
+        exit(2)
+
+    for j in regex_json:
+        if ['regex', 'flags']!=list(j.keys()):
+            print("[!] %s does not fit the regex format!"%(i))
+            exit(2)
+
+print("[+] Regexps checks passed")
+
+try:
+    asn_list=json.load(open("sources/raw/asn-list.json"))
+except Exception as e:
+    print("[!] Error in decoding asn list!")
+    print("[!] Exception: %s"%(e.msg))
+    exit(2)
+
+for k,v in asn_list.items():
+    if ['match', 'reject']!=list(v.keys()):
+        print("[!] %s does not fit the asn format!"%(i))
+        exit(2)
+
+    if not isinstance(v["match"],list):
+        print("[!] The value of match is not a list!")
+        print("[!] Key name: %s"%(k))
+        exit(2)
+
+    if not isinstance(v["reject"],list):
+        print("[!] The value of reject is not a list!")
+        print("[!] Key name: %s"%(k))
+        exit(2)
+
+print("[+] ASN list checks passed")
--- validate-lists.sh
+++ validate-lists.sh
@@ -7,3 +7,4 @@ set -o pipefail
 ./scripts/ipv4-checker.py
 ./scripts/ipv6-checker.py
 ./scripts/new-line-checker.py
+./scripts/check-syntax-and-clean-json.py
