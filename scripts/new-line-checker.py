d46dc5c43f95b6e1d515941a9d711a5d98f4a8f1
Mo Langning <133737702+molangning@users.noreply.github.com>
2024-01-30 08:00:21 +0800

    Update new-line-checker.py

--- scripts/new-line-checker.py
+++ scripts/new-line-checker.py
@@ -6,10 +6,14 @@ print("[+] New line check")
 
 files = []
 dirs = ['sources/','blacklists/','whitelists/']
+ignore_file_exts = [".json"]
 
 for i in dirs:
     for root,_,file_list in os.walk(i):
         for file in file_list:
+            for ignore_ext in ignore_file_exts:
+                if file.endswith(ignore_ext):
+                    continue
             files.append(os.path.join(root,file))
 
 for i in files:
