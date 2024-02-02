a4e8cbf7f54860dc572191cddbbf8f3f428349a6
Mo Langning <133737702+molangning@users.noreply.github.com>
2024-02-02 11:42:19 +0800

    Update update-30-minutes.sh

--- updaters/update-30-minutes.sh
+++ updaters/update-30-minutes.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -e 
-set -o pipefail
-
 ./scripts/get-tor-exit-list.py
 ./scripts/get-ip-range-from-isc.py
 ./scripts/get-ip-range-from-asn.py
+
+set -e 
+set -o pipefail
 
 ./validate-lists.sh
