08f4b919a53d109807094e8b71e9160968f2721a
Mo Langning <133737702+molangning@users.noreply.github.com>
2024-05-15 10:05:16 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/bash
 
-# Script to update lists for github
+# DO NOT USE THIS SCRIPT WITH USER INPUT!!!
+# COMMAND INJECTION IS POSSIBLE
 
-git add -N .
+set -euxo pipefail
 
-if [ -z "$(git ls-files --modified sources/)" ]; then
+if [ -z "$(git ls-files --modified $2)" ]; then
     echo "[+] No files were changed"
 else
     echo "[+] Files were changed! Pushing changed..."
-    git stage sources/
-    git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY
-    git config --local user.email "example@github.com"
-    git config --local user.name "GitHub Action"
+    git pull
+    git stage $2
+    git remote set-url origin https://x-access-token:$1@github.com/$GITHUB_REPOSITORY
+    git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+    git config --local user.name "GitHub Actions"
     git commit -m "[Github Action] Automated lists update."
     git push
 fi
-
